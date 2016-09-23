a4fdd4dc548b4dc48ed33feae2bf8d6bddd2d311
kcao <kcao@b6bc4755-4ae6-429a-979a-b16a02a10f27>
2016-09-23 05:58:42 +0000

    创建hbase表结构    清洗联通位置数据

---
+++ src/test/com/soda/test/Test.scala
@@ -0,0 +1,14 @@
+package com.soda.test
+
+/**
+  * Created by Administrator on 2016/9/23.
+  */
+object Test {
+
+  def main(args: Array[String]) {
+    Range(10,-1,-1).foreach(e => println(e))
+
+
+  }
+
+}
