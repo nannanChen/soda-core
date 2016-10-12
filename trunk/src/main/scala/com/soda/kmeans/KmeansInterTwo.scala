548dc89c7cf5b57a327dcb0561c0d1195c1b4f00
haohaizi <haohaizi@b6bc4755-4ae6-429a-979a-b16a02a10f27>
2016-10-12 06:51:40 +0000

    优化

--- trunk/src/main/scala/com/soda/kmeans/KmeansInterTwo.scala
+++ trunk/src/main/scala/com/soda/kmeans/KmeansInterTwo.scala
@@ -7,8 +7,6 @@ import org.apache.spark.mllib.linalg.{Vector, Vectors}
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
-import scala.collection.immutable.HashMap
-
 /**
   * Created by nan on 2016/9/27 9:43
   *
@@ -41,20 +39,30 @@ object KmeansInterTwo {
 
     val conf = new SparkConf().setAppName("KmeasInterTwo")
     val sc = new SparkContext(conf)
-//    sc.setLocalProperty("file.encoding","GB2312")
     val rawTrainingData = sc.textFile("hdfs://192.168.20.90:9000/soda/fusai/biaoerutf8_1.csv")
-
-    val parsedTrainingData = rawTrainingData.filter(!isColumnNameLine(_)).filter((_.replace("\"","").split(",").length==29)).map(line=>{
-      val splited= line.replaceAll("\"","").split(",")
-      val iemi = HashAlgorithmsUtil.BKDRHash(splited(0))
-      val brand = getBrand(splited(4))
-      val x = line.replaceAll("\"","").replace(splited(0),iemi.toString).replaceFirst(splited(4),brand).replace(splited(5),"0").replace("男","0").replace("女","1")
-        .replace("不详","2").replace("低","0").replace("中","1").replace("高","2").replace(splited(11),"0").replace(splited(12),"0")
-      val y = x.split(",").drop(6).mkString(",")
-        Vectors.dense(y.split(",").map(_.trim).filter(!"".equals(_)).map(_.toDouble)) //org.apache.spark.mllib.linalg.Vector
+      val trainingData = cleanData(rawTrainingData).map(x=>{
+      Vectors.dense(x._2)
     })
 
-    val map1=rawTrainingData.filter(!isColumnNameLine(_)).filter((_.replace("\"","").split(",").length==29)).map(line=>{
+    val txt = cleanData(rawTrainingData).map(x=>{
+      x._2.mkString(",")
+    }).saveAsTextFile("hdfs://192.168.20.90:9000/soda/fusai/test")
+//    val map1 = cleanData(rawTrainingData)
+//    val numClusters = Array(10)
+//    val numIteration = 10
+//    val runTimes = 3
+//    println("----------------START-------------------")
+//    numClusters.foreach(f=>{
+//      kmeans(trainingData,f,numIteration,map1)
+//    })
+    /**
+      * 根据以上结果输入数据
+      */
+    println("Spark MLlib K-means clustering test finished.")
+  }
+
+  def cleanData(rdd: RDD[String]): RDD[(String,Array[Double])] ={
+    val map=rdd.filter(!isColumnNameLine(_)).filter((_.replace("\"","").split(",").length==29)).map(line=>{
       val splited= line.replaceAll("\"","").split(",")
       val iemi = HashAlgorithmsUtil.BKDRHash(splited(0))
       val brand = getBrand(splited(4))
@@ -63,21 +71,9 @@ object KmeansInterTwo {
       val y = x.split(",").drop(6).mkString(",")
       (splited(0),y.split(",").map(_.trim).filter(!"".equals(_)).map(_.toDouble)) //org.apache.spark.mllib.linalg.Vector
     })
-
-
-    val numClusters = Array(10)
-    val numIteration = 10
-    val runTimes = 3
-
-    println("----------------START-------------------")
-    numClusters.foreach(f=>{
-      kmeans(parsedTrainingData,f,numIteration,map1)
-    })
-    /**
-      * 根据以上结果输入数据
-      */
-    println("Spark MLlib K-means clustering test finished.")
+    return map
   }
+
   def kmeans(parsedTrainingData: RDD[Vector], numClusters:Int, numIteration:Int, map:RDD[(String,Array[Double])]): Unit ={
     var clusterIndex = 0
     val cluster = KMeans.train(parsedTrainingData,numClusters,numIteration)
@@ -90,33 +86,18 @@ object KmeansInterTwo {
     })
     parsedTrainingData.collect().foreach(testDataLine => {
       testDataLine.toArray.mkString(",")
-//     val map2 = map.foreach(x=>{
-//        println("x._1:"+x._1+"\tx._2:"+x._2.mkString(","))
-//      })
-//
-//      println("map:"+map2)
       val predictedClusterIndex = cluster.predict(testDataLine)
       val iemi = map.filter(f=>{
         val s1="["+f._2.mkString(",")+"]";
         val s2=testDataLine.toString
         val rs=s1.equals(s2)
-//        println("s1="+s1+" equals s2="+s2+" rs="+rs)
         rs
       }).map(_._1).foreach(x=>{
         RedisService.addTag(x,predictedClusterIndex+"")
       })
-//      println("iemi:"+iemi)
-//      println("-------这条数据:"+testDataLine.toString + "属于:"+predictedClusterIndex)
+
     })
-//    val ks:Array[Int] = Array(10)
-//    ks.foreach(cluster => {
-//      val model:KMeansModel = KMeans.train(parsedTrainingData, cluster,numIteration)
-//      val ssd = model.computeCost(parsedTrainingData)
-//      println("sum of squared distances of points to their nearest center when k=" + cluster + " -> "+ ssd)
-//    })
   }
-
-
   /**
     * 判断该行是否含有某字符串
     *
@@ -138,3 +119,14 @@ object KmeansInterTwo {
   }
 
 }
+
+
+//val parsedTrainingData = rawTrainingData.filter(!isColumnNameLine(_)).filter((_.replace("\"","").split(",").length==29)).map(line=>{
+//val splited= line.replaceAll("\"","").split(",")
+//val iemi = HashAlgorithmsUtil.BKDRHash(splited(0))
+//val brand = getBrand(splited(4))
+//val x = line.replaceAll("\"","").replace(splited(0),iemi.toString).replaceFirst(splited(4),brand).replace(splited(5),"0").replace("男","0").replace("女","1")
+//.replace("不详","2").replace("低","0").replace("中","1").replace("高","2").replace(splited(11),"0").replace(splited(12),"0")
+//val y = x.split(",").drop(6).mkString(",")
+//Vectors.dense(y.split(",").map(_.trim).filter(!"".equals(_)).map(_.toDouble)) //org.apache.spark.mllib.linalg.Vector
+//})
